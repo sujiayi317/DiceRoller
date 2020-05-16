48ca592bbe78bb2116a9771c6b020cdfae844249
sujiayi317 <sujiayi317@gmail.com>
2020-05-16 13:49:07 -0400

    add comments

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -9,9 +9,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //specify which layout is associated with the activity, and you inflate the layout:
         setContentView(R.layout.activity_main)
 
+        // get a reference to the View with a specific ID:
         val rollButton: Button = findViewById(R.id.roll_button)
+        /*A click handler is a method that is invoked each time the user clicks or taps on
+        a clickable UI element, such as a button. To create a click handler you need:
+        1.A method that performs some operation.
+        2.The setOnClickListener() method, which connects the Button to the handler method.*/
         rollButton.setOnClickListener { rollDice() }
     }
 
